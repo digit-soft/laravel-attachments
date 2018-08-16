bd14e4a3f7c5fc62218c276323c66188964e7ad7
digit <digit.vova@gmail.com>
2018-08-16 16:59:49 +0300

    Attachments facade update

--- src/Facades/Attachments.php
+++ src/Facades/Attachments.php
@@ -7,16 +7,16 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Attachments
  * @package DigitSoft\Attachments\Facades
- * @method static \DigitSoft\Attachments\Attachment createFromFile(\Illuminate\Http\File $file, $group, $private = false)
+ * @method static \DigitSoft\Attachments\Attachment createFromFile(\Illuminate\Http\File|\Illuminate\Http\UploadedFile $file, $group, $private = false)
  * @method static array saveFile(\Illuminate\Http\UploadedFile $uploadedFile, $group, $private = false)
  * @method static void addUsage(\DigitSoft\Attachments\Attachment $attachment, $model_id, $model_type)
  * @method static void removeUsage(\DigitSoft\Attachments\Attachment $attachment, $model_id, $model_type)
  * @method static bool hasUsage(\DigitSoft\Attachments\Attachment $attachment, $model_id, $model_type)
  * @method static string getUrl(\DigitSoft\Attachments\Attachment $attachment)
  * @method static string getSavePath($type = 'public', $group = null, $full = false)
+ * @method static void cleanUp($expire_time = null, $onlyDb = false)
  * @method static \Illuminate\Contracts\Filesystem\Filesystem|\Illuminate\Filesystem\FilesystemAdapter getStoragePublic()
  * @method static \Illuminate\Contracts\Filesystem\Filesystem|\Illuminate\Filesystem\FilesystemAdapter getStoragePrivate()
- * @method static void cleanUp($expire_time = null, $onlyDb = false)
  * @see \DigitSoft\Attachments\AttachmentsManager
  */
 class Attachments extends Facade
