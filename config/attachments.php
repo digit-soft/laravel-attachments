cf97098e247b42c8e2a025c891f52155616658df
digit <digit.vova@gmail.com>
2018-08-17 15:22:25 +0300

    default config changes

--- config/attachments.php
+++ config/attachments.php
@@ -17,17 +17,40 @@ return [
     'save_path_private' => 'private/attachments',
     /*
     |------------------------------------------------------
+    | Path for saving image cache (resized images)
+    |------------------------------------------------------
+    | Relative to storage/app dir
+    */
+    'image_cache_path' => 'public/images',
+    /*
+    |------------------------------------------------------
     | Absolute URL settings
     |------------------------------------------------------
     | If null system settings will be used
     */
     'url' => [
-        'host' => null,
-        'scheme' => null,
-        'base_path' => 'storage/attachments',
-        'private_route' => 'attachments/download'
+        'host' => null,                             //host for file downloads
+        'scheme' => null,                           //scheme for file download
+        'base_path' => 'storage/attachments',       //base path for file download
+        'private_route' => 'attachments/download'   //route name for private files download
     ],
+    /*
+    |------------------------------------------------------
+    | Storage disk for public files
+    |------------------------------------------------------
+    */
     'public_storage' => 'local',
+    /*
+    |------------------------------------------------------
+    | Storage disk for private files
+    |------------------------------------------------------
+    */
     'private_storage' => 'local',
+    /*
+    |------------------------------------------------------
+    | Attachments expire time
+    |------------------------------------------------------
+    | Time after what attachments without usage will be deleted
+    */
     'expire_time' => 10800,
 ];
