310c3b4d56d856c77836ef4069d7b2032cd03620
digit <digit.vova@gmail.com>
2018-09-05 10:52:37 +0300

    AttachmentUsage model update

--- src/AttachmentUsage.php
+++ src/AttachmentUsage.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * DigitSoft\Attachments\AttachmentUsage
  *
- * @property int    $attachment_id Attachment
- * @property string $model_id Model ID
- * @property string $model_type Model type
+ * @property int             $attachment_id Attachment
+ * @property string          $model_id Model ID
+ * @property string          $model_type Model type
+ * @property-read Attachment $attachment
+ * @property-read Model      $model
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereAttachmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereModelId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereModelType($value)
@@ -17,5 +19,25 @@ use Illuminate\Database\Eloquent\Model;
  */
 class AttachmentUsage extends Model
 {
+    public $timestamps = false;
+
     protected $fillable = ['attachment_id', 'model_id', 'model_type'];
+
+    /**
+     * Get attachment
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function attachment()
+    {
+        return $this->belongsTo(Attachment::class, 'attachment_id', 'id');
+    }
+
+    /**
+     * Get model using attachment
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function model()
+    {
+        return $this->morphTo('model');
+    }
 }
