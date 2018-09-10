b063923a4ade60749422609b5b783f42a0e3ed14
modelfak <modelfak@gmail.com>
2018-09-10 17:50:26 +0300

    Add events for attachable models

--- src/AttachmentUsage.php
+++ src/AttachmentUsage.php
@@ -7,12 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * DigitSoft\Attachments\AttachmentUsage
  *
- * @property int             $id ID
- * @property int             $attachment_id Attachment
- * @property string          $model_id Model ID
- * @property string          $model_type Model type
- * @property-read Attachment $attachment
- * @property-read Model      $model
+ * @property int $attachment_id Attachment
+ * @property string $model_id Model ID
+ * @property string $model_type Model type
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereAttachmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereModelId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|AttachmentUsage whereModelType($value)
@@ -20,25 +17,9 @@ use Illuminate\Database\Eloquent\Model;
  */
 class AttachmentUsage extends Model
 {
+    protected $primaryKey = 'attachment_id';
+
     public $timestamps = false;
 
     protected $fillable = ['attachment_id', 'model_id', 'model_type'];
-
-    /**
-     * Get attachment
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function attachment()
-    {
-        return $this->belongsTo(Attachment::class, 'attachment_id', 'id');
-    }
-
-    /**
-     * Get model using attachment
-     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
-     */
-    public function model()
-    {
-        return $this->morphTo('model');
-    }
 }
