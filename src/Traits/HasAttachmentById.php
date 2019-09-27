26d33bb2bff0c2585e3ac2a64cb8d0302390b7ca
digit <digit.vova@gmail.com>
2019-09-27 17:26:43 +0300

    HasAttachmentById trait CS

--- src/Traits/HasAttachmentById.php
+++ src/Traits/HasAttachmentById.php
@@ -8,19 +8,20 @@ use DigitSoft\Attachments\Attachment;
  * Trait HasAttachmentById.
  * Use this trait if you have attachment ID field in your model.
  *
- * @package DigitSoft\Attachments\Traits
- * @property string          $attachment_id_attribute Attachment ID db attribute (use it if you want to override attribute with attachment ID)
+ * @property      string     $attachment_id_attribute Attachment ID db attribute (use it if you want to override attribute with attachment ID)
  * @property-read Attachment $attachment
  */
 trait HasAttachmentById
 {
     /**
      * Get used attachment (one) by ID field
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function attachment()
     {
         $idAttribute = $this->attachment_id_attribute ?? 'attachment_id';
+
         return $this->belongsTo(Attachment::class, $idAttribute, 'id');
     }
 }
